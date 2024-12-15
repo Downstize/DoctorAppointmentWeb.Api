1f62261d39f0f762ef2fddca6395e5551c16bbac
downstize <swankydid@gmail.com>
2024-12-15 18:29:11 +0300

    Change contract request of appointment for better compatibility with websocket

--- Requests/AppointmentRequest.cs
+++ Requests/AppointmentRequest.cs
@@ -16,9 +16,9 @@ namespace DoctorAppointmentWebApi.DTOs
         [Required(ErrorMessage = "Дата и время записи обязательны.")]
         public DateTime AppointmentDateTime { get; set; }
 
-        [Required(ErrorMessage = "Статус записи обязателен.")]
-        [StringLength(50, ErrorMessage = "Длина статуса не может превышать 50 символов.")]
-        public string Status { get; set; }
+        // [Required(ErrorMessage = "Статус записи обязателен.")]
+        // [StringLength(50, ErrorMessage = "Длина статуса не может превышать 50 символов.")]
+        // public string Status { get; set; }
 
         [StringLength(500, ErrorMessage = "Длина заметок не может превышать 500 символов.")]
         public string Notes { get; set; }
